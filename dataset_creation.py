33426d1e6846c44df73f66e3e1ab0bf1aa1cf48a
David Anders <danders232@gmail.com>
2023-06-15 22:39:22 +0200

    misc: Add new league and comps in dataset creation

--- dataset_creation.py
+++ dataset_creation.py
@@ -3,10 +3,13 @@ from match_data_retrieval import *
 LEAGUES_API_CODE = {"Bundesliga" : "78",
            "Premier League" : "39",
            "La Liga" : "140",
-           "Serie A" : "135"
+           "Serie A" : "135",
+           "Ligue 1" : "61"
            }
 
-INTERNATIONAL_COMPETITIONS_API_CODE = {"UEFA Champions League" : "2"}
+INTERNATIONAL_COMPETITIONS_API_CODE = {"UEFA Champions League" : "2",
+                                        "UEFA Europa League" : "3"
+                                      }
 
 # Webscraping from oddsportal.com requires a different formatting of the league and seasons strings
 COUNTRY_AND_LEAGUES_ODDS_PORTAL = {"germany" : "bundesliga", 
@@ -19,7 +22,7 @@ COUNTRY_AND_LEAGUES_ODDS_PORTAL = {"germany" : "bundesliga",
 SEASONS_ODDS_PORTAL = ["2021-2022", "2020-2021", "2019-2020", "2018-2019", "2017-2018", "2016-2017", "2015-2016", "2014-2015", "2013-2014", "2012-2013", "2011-2012",
            "2010-2011"]
 
-SEASONS_API = [str(year) for year in range(2010, 2023)]
+SEASONS_API = [str(year) for year in range(2015, 2019)]
 
 
 def compile_data_for_leagues_and_seasons(leagues, seasons):
@@ -29,5 +32,16 @@ def compile_data_for_leagues_and_seasons(leagues, seasons):
             retrieve_competition_season_data(competition_name=league, competition_api_id=LEAGUES_API_CODE.get(league), season=season)
 
 
+def compile_data_for_internationalcomp_and_seasons(international_comp, seasons):
+    for comp in international_comp:
+        for season in seasons:
+            print(f"Getting data for {comp} in season {season}")
+            retrieve_competition_season_data(competition_name=comp, competition_api_id=INTERNATIONAL_COMPETITIONS_API_CODE.get(comp), season=season)
+
+
+
 
 compile_data_for_leagues_and_seasons(LEAGUES_API_CODE, SEASONS_API)
+#compile_data_for_internationalcomp_and_seasons(INTERNATIONAL_COMPETITIONS_API_CODE, SEASONS_API)
+
+
